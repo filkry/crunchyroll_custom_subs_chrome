d89a767d338d2e71d7aa786779bb9abd03f28184
Fil Krynicki <filipkrynicki@gmail.com>
2020-07-29 21:12:06 -0700

    Able to run content script on video iframe

---
+++ contentscript.js
@@ -0,0 +1,9 @@
+console.log("content script running");
+
+//let video_frame = document.getElementById("vilos-player");
+
+//let video_document = video_frame.contentWindow.document;
+
+let video = document.getElementById("player0");
+
+video.style.display = "none";
