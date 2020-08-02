26e74f16871076a2a422ecd5edeee52c46253df8
Fil Krynicki <filipkrynicki@gmail.com>
2020-08-01 18:57:44 -0700

    Starting to parse the subtitle file

--- contentscript_outsidevideo.js
+++ contentscript_outsidevideo.js
@@ -3,6 +3,8 @@ function preventDefaults (e) {
   e.stopPropagation();
 };
 
+let loadedsubs = [];
+
 let videoarea = document.getElementById("showmedia_video");
 if(videoarea != null) {
     // -- create subtitle upload/control area
@@ -22,12 +24,50 @@ if(videoarea != null) {
     })
 
     controlsdiv.addEventListener("drop", function(dragevent) {
-        console.log("drop event");
+        //console.log("drop event");
         dragevent.preventDefault();
         dragevent.stopPropagation();
         if(dragevent.dataTransfer.files.length > 0) {
             let file = dragevent.dataTransfer.files[0];
             console.log(file.name);
+
+            let filereader = new FileReader();
+            filereader.onload = function(event) {
+                //console.log(filereader.result);
+
+                let lines = filereader.result.split('\n');
+
+                const EParseStates = {
+                    eNone: 0,
+                    eWaitingForNextSub: 1,
+                    eWaitingForTime: 2,
+                    eReadingLines: 0,
+                };
+                Object.freeze(EParseStates);
+
+                let state = EParseStates.eWaitingForNextSub;
+
+                let matchnewsub = /^[0-9]+$/;
+
+                loadedsubs = []; // reset
+
+                lines.forEach(line => {
+                    line = line.replace('\r', '');
+
+                    if(state == EParseStates.eWaitingForNextSub) {
+                        let res = line.match(matchnewsub);
+                        if(res != null && res.length > 0) {
+                            loadedsubs.push({}); // append empty sub
+                            state = EParseStates.eWaitingForTime;
+                            console.log("New sub!");
+                        }
+                    }
+                    else if(state == EParseStates.eWaitingForTime) {
+
+                    }
+                });
+            };
+            filereader.readAsText(file);
         }
     });
 
